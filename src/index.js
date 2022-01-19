c463d36e28e46583d0c9c0b14674a703f723c02f
murat <arkantarkan.33@gmail.com>
2022-01-19 22:09:06 +0000

    first

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "alertifyjs/build/css/alertify.min.css";
+import "react-toastify/dist/ReactToastify.min.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.min.css";
 import "alertifyjs/build/css/alertify.min.css";
 
 import "./assets/css/style.css";
-import { StoreProvider } from './store';
+import { StoreProvider } from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
     <StoreProvider>
       <App />
     </StoreProvider>
-
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
